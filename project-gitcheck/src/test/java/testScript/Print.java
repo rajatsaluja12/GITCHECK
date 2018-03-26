24187ab6a150127619efa0e34919fa88369643c4
temp <temp@rajat-PC>
2018-03-26 21:23:58 +0530

    Committed

--- project-gitcheck/src/test/java/testScript/Print.java
+++ project-gitcheck/src/test/java/testScript/Print.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Print {
 	@Test
 	public void Cognizant() {
-		System.out.println("Hence proved");
+		System.out.println("Hence proved rajat is nice boy");
 	}
 
 }
