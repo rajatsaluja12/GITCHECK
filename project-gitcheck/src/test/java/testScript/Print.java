6f51aeba6069dc01bce4513a69efbb7b6eccc896
rajatsaluja12 <37304494+rajatsaluja12@users.noreply.github.com>
2018-03-26 21:24:52 +0530

    Update Print.java

--- project-gitcheck/src/test/java/testScript/Print.java
+++ project-gitcheck/src/test/java/testScript/Print.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Print {
 	@Test
 	public void Cognizant() {
-		System.out.println("Hence proved rajat is nice boy");
+		System.out.println("Hence proved rajat is nice HOOD");
 	}
 
 }
