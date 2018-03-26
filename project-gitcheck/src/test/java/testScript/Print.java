41d642bbb256f2f6030bd1a6cd3d61db90bb5bd1
temp <temp@rajat-PC>
2018-03-26 21:36:47 +0530

    Commit new

--- project-gitcheck/src/test/java/testScript/Print.java
+++ project-gitcheck/src/test/java/testScript/Print.java
@@ -3,9 +3,15 @@ package testScript;
 import org.testng.annotations.Test;
 
 public class Print {
-	@Test
+	@Test(priority=0)
 	public void Cognizant() {
-		System.out.println("Hence proved rajat is nice HOOD");
+		System.out.println("Cognizant pays well");
 	}
+
+	@Test(priority=1)
+	public void TCS() {
+		System.out.println("tcs is good employee friendly organisation");
+	}
+
 
 }
