d83cd54196b8145212160ad976cc04152a35d6e4
temp <temp@rajat-PC>
2018-04-06 21:03:57 +0530

    das

--- project-gitcheck/src/test/java/testScript/Print.java
+++ project-gitcheck/src/test/java/testScript/Print.java
@@ -5,14 +5,14 @@ import org.testng.annotations.Test;
 public class Print {
 	@Test(priority=0)
 	public void Cognizant() {
-		System.out.println("Cognizant pays well");
+		System.out.println("Cognizant pays well new");
 	}
 
 	@Test(priority=1)
 	public void TCS() {
 		System.out.println("tcs is good employee friendly organisation");
 	}
-	
+
 	@Test(priority=2)
 	public void Mphasis() {
 		System.out.println("Mphasis pay me to run my house");
