75bbb1b5176a31995b46ce8db7221025183b3a2b
rajat.saluja12 <er.rajatsaluja@gmail.com>
2018-04-22 09:57:33 +0530

    commit performed

--- project-gitcheck/src/test/java/testScript/Print.java
+++ project-gitcheck/src/test/java/testScript/Print.java
@@ -10,7 +10,7 @@ public class Print {
 
 	@Test(priority=1)
 	public void TCS() {
-		System.out.println("tcs is good employee friendly organisation");
+		System.out.println("tcs is good employee friendly organisation as per my knowledge");
 	}
 
 	@Test(priority=2)
