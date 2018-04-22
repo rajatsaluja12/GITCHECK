954174ff9e02e38c422eb0ab05f10f7477376c74
rajat.saluja12 <er.rajatsaluja@gmail.com>
2018-04-22 10:08:36 +0530

    commit new

--- project-gitcheck/src/test/java/testScript/Print.java
+++ project-gitcheck/src/test/java/testScript/Print.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 public class Print {
 	@Test(priority=0)
 	public void Cognizant() {
-		System.out.println("Cognizant pays well new");
+		System.out.println("Cognizant pays well");
 	}
 
 	@Test(priority=1)
 	public void TCS() {
-		System.out.println("tcs is good employee friendly organisation as per my knowledge");
+		System.out.println("tcs ");
 	}
 
 	@Test(priority=2)
